32b5a06e19655743300a6bd58e6b55cf682ce821
zxc <66sunfly99@gmail.com>
2024-09-25 15:02:23 +0500

    Task 1.6

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+document.writeln("Мороз и солнце; день чудесный!<br>");
+document.writeln("Еще ты дремлешь, друг прелестный —<br>");
+document.writeln("Пора, красавица, проснись:<br>");
+document.writeln("Открой сомкнуты негой взоры<br>");
+document.writeln("Навстречу северной Авроры,<br>");
+document.writeln("Звездою севера явись!<br>");
+document.writeln("...<br>");
